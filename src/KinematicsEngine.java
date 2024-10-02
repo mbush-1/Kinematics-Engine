15877bbb8784da22958216912e98add4b5bc6079
mbush-1 <mbush1@ocdsb.ca>
2024-10-02 19:03:23 -0400

    made some progress on velocity and displacement... running into some issues..

--- src/KinematicsEngine.java
+++ src/KinematicsEngine.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class KinematicsEngine {
 
     int tick;
+    double timeElapsed; // in s
+    Object object;
 
     // "main"
     static void runSimulator() {
         KinematicsEngine kinematicsEngine = new KinematicsEngine();
         kinematicsEngine.registerValues();
+        kinematicsEngine.initializeObjects();
+        for (int i = 0; i <= 50; i++) {
+            kinematicsEngine.runTick();
+        }
     }
 
     // [FIELDS]
@@ -63,14 +69,45 @@ public class KinematicsEngine {
     }
 
     void initializeObjects() {
-        Ball object = new Ball(startHeight);
+        object = new Ball(startHeight);
     }
 
     void simulateGravity() {
+        while (true) {
 
+        }
     }
 
     void runTick() {
 
+        // 1 time
+        tick += 1;
+        timeElapsed = (tick) / (Math.pow(10, timePrecision));
+
+        // 2 velocity
+        final double GRAVITY_ACCELERATION_BY_TIME = GRAVITY / (Math.pow(10, timePrecision));
+        double proposedVelocity = object.instantaneousVelocity + GRAVITY_ACCELERATION_BY_TIME;
+
+        // 3 displacement (might have to rework for bouncing)
+        double proposedDisplacement = ((proposedVelocity) / 2.0) * (timeElapsed);
+
+
+//        proposedVelocity = Math.round(proposedVelocity * Math.pow(10, timePrecision)) / Math.pow(10, timePrecision);
+//        proposedDisplacement = Math.round(proposedDisplacement * Math.pow(10, timePrecision)) / Math.pow(10, timePrecision);
+
+
+        // check if d<=0
+
+        object.instantaneousVelocity = proposedVelocity;
+        object.displacement = proposedDisplacement;
+
+        printValues();
+
+    }
+
+    void printValues() {
+        System.out.println(
+                "TICK: " + tick + " | ELAPSED TIME: " + timeElapsed + " | VELOCITY: " + object.instantaneousVelocity + " | HEIGHT: "  + (object.displacement + startHeight ) + "|"
+        );
     }
 }
